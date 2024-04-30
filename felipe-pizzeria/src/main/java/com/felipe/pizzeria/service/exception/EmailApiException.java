e1a41842827593f1e31de8fce83a7dc530feccbd
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 17:31:00 -0500

    [Created email exception]

---
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/exception/EmailApiException.java
@@ -0,0 +1,8 @@
+package com.felipe.pizzeria.service.exception;
+
+public class EmailApiException extends RuntimeException{
+
+    public EmailApiException(){
+        super("Error sending email...");
+    }
+}
