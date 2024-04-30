8187ad75cd960b2be1df579183aba7fb599ecd29
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 16:47:06 -0500

    [add query Methods to Pizzas- PizzaRepository]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/persistence/repository/PizzaRepository.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/persistence/repository/PizzaRepository.java
@@ -1,7 +1,12 @@
 package com.felipe.pizzeria.persistence.repository;
 
 import com.felipe.pizzeria.persistence.entity.PizzaEntity;
+import com.felipe.pizzeria.service.dto.UpdatePizzaPriceDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +22,15 @@ public interface PizzaRepository extends ListCrudRepository <PizzaEntity, Intege
 
     List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
     int countByVeganTrue();
+
+    //metodo que permite actualizar el precio de una pizza desde un sql nativo
+    @Query(value =
+            "UPDATE pizza"+
+            "SET price = :#{#newPizzaPrice.newPrice} "+
+            "WHERE id_pizza = :#{#newPizzaPrice.pizzaId} ", nativeQuery = true)
+    @Modifying
+    void updatePrice(@Param("newPizzaPrice") UpdatePizzaPriceDto newPizzaPrice);
+
 
 
 }
