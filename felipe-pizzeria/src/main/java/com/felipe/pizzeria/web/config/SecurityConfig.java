d40a52b4c1aed1abd39d605ca2064a0b02df7b92
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 18:04:24 -0500

    [add Request Matechers]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/web/config/SecurityConfig.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.felipe.pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll()
+                .requestMatchers(HttpMethod.PUT).denyAll()
                 .anyRequest()
                 .authenticated()
                 .and()
