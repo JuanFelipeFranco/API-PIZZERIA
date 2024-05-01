bf987e87ea404eddcdbbecc53f3ceaeef3281a10
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 21:13:57 -0500

    [user audit in the database]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/persistence/audit/AuditableEntity.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.felipe.pizzeria.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -15,4 +17,13 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDated;
+
+    @Column(name="created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name="modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
+
 }
