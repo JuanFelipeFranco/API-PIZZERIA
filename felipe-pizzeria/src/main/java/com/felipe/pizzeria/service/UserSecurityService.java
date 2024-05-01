65c4d4e1bf6624966caa7a6b7344d3fbdce3de58
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 19:25:53 -0500

    [Apply authorities to user - security permissions ]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
@@ -4,11 +4,16 @@ import com.felipe.pizzeria.persistence.entity.UserEntity;
 import com.felipe.pizzeria.persistence.entity.UserRoleEntity;
 import com.felipe.pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserSecurityService implements UserDetailsService {
@@ -31,9 +36,37 @@ public class UserSecurityService implements UserDetailsService {
             return User.builder()
                     .username(userEntity.getUsername())
                     .password(userEntity.getPassword())
-                    .roles(roles)
+                    .authorities(this.grantedAuthorities(roles))
                     .accountLocked(userEntity.getLocked())
                     .disabled(userEntity.getDisabled())
                     .build();  //convierte el builder en UserDetails.
+    }
+
+    //metodo que retorna los permisos especificos que vamos a tener
+    private String[] getAuthorities(String role){
+        if ("ADMIN".equals(role) || "CUSTOMER".equals(role)){
+            return new String[] {"random_order"}; //el nombre de nuestro permiso.
+        }
+        //si no es administrador o cliente
+        return new String[] {};
+    }
+
+    //metodo privado que devuelve una lista de grantedauthorities, va recibir el array de string caprutado en roles.
+    private List<GrantedAuthority> grantedAuthorities(String[] roles) {
+
+        //creamos una lista cuyo tamaño es del tamaño de los roles
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+
+        //ciclo for each a los roles que tengo.
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+            //hacemos este bucle donde hacemos un llamado al metodfo que creamos y le enviamos el role que obtuvimos en el ciclo anterior
+            for (String authority : this.getAuthorities(role)) {
+                //authority permiso plano
+                authorities.add(new SimpleGrantedAuthority(authority));
+            }
+        }
+        return authorities;
     }
 }
