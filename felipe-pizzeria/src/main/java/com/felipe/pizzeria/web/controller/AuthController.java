bf2b077e80f58d99d208cbf883ae52e2f9834606
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 20:17:19 -0500

    [Created JWT  when a user logs in]

---
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/web/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.felipe.pizzeria.web.controller;
+
+import com.felipe.pizzeria.service.dto.LoginDto;
+import com.felipe.pizzeria.web.config.JwtUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(LoginDto loginDto){
+        //creamos el UsernameAuthenticationToken a partir del usuario y contraseña que nos llega de loginDto
+        UsernamePasswordAuthenticationToken login = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        //creamos objeto de autenticacion, que tiene el llamado al autenticate del autenticaManager;  autenticando al usuario
+        Authentication authentication = this.authenticationManager.authenticate(login);
+        System.out.println(authentication.isAuthenticated());
+        System.out.println(authentication.getPrincipal()); //muestra el usuario por consola
+
+        //creando el JWT que queremos asignar al usuario y enviarselo como respuesta a la peticion
+        String jwt = this.jwtUtil.create(loginDto.getUsername());
+
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, jwt).build();
+    }
+}
