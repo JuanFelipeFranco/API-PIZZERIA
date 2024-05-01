27057dead8ff8655d710d6c1af5a586bc731437c
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 19:19:32 -0500

    [assign roles to user]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.felipe.pizzeria.service;
 
 import com.felipe.pizzeria.persistence.entity.UserEntity;
+import com.felipe.pizzeria.persistence.entity.UserRoleEntity;
 import com.felipe.pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -23,12 +24,16 @@ public class UserSecurityService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
             UserEntity userEntity = this.userRepository.findById(username)
                 .orElseThrow(()-> new UsernameNotFoundException("User "+ username + "not found"));
-        return User.builder()
-                .username(userEntity.getUsername())
-                .password(userEntity.getPassword())
-                .roles("ADMIN")
-                .accountLocked(userEntity.getLocked())
-                .disabled(userEntity.getDisabled())
-                .build();  //convierte el builder en UserDetails.
+
+            //de la clase userEntity obtenemos los roles que son de tipo lista le hacemos un map para sacar unicamente el STRING que contiene el rol y ese lo convierto en un array
+            String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
+            return User.builder()
+                    .username(userEntity.getUsername())
+                    .password(userEntity.getPassword())
+                    .roles(roles)
+                    .accountLocked(userEntity.getLocked())
+                    .disabled(userEntity.getDisabled())
+                    .build();  //convierte el builder en UserDetails.
     }
 }
