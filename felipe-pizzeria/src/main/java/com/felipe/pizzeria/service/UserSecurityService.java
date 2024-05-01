c96591d41a34c0b53e7dd89501f0257a7e9aa31c
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-04-30 20:19:47 -0500

    [verify user entity print in terminal]

--- felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
+++ felipe-pizzeria/src/main/java/com/felipe/pizzeria/service/UserSecurityService.java
@@ -30,6 +30,8 @@ public class UserSecurityService implements UserDetailsService {
             UserEntity userEntity = this.userRepository.findById(username)
                 .orElseThrow(()-> new UsernameNotFoundException("User "+ username + "not found"));
 
+            System.out.println(userEntity);
+
             //de la clase userEntity obtenemos los roles que son de tipo lista le hacemos un map para sacar unicamente el STRING que contiene el rol y ese lo convierto en un array
             String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
 
